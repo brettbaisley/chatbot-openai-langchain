3ffab5cd4573fa5153d4e3bb0a7ad617556b02bf
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2024-10-31 21:38:46 -0400

    Minor cleanup

--- query2.py
+++ query2.py
@@ -2,8 +2,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema import StrOutputParser
-from langchain.prompts.chat import HumanMessagePromptTemplate, SystemMessagePromptTemplate
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import PromptTemplate
 
 template: str = """/
     You are a customer support specialist.
@@ -17,20 +16,6 @@ template: str = """/
     Question: {question}. 
     """
 
-system_message_prompt_template = SystemMessagePromptTemplate.from_template(template)
-
-human_message_prompt_template = HumanMessagePromptTemplate.from_template(
-    input_variables=["question", "context"], 
-    template="{question}"
-    )
-
-chat_prompt_template = ChatPromptTemplate.from_messages([
-    system_message_prompt_template, 
-    human_message_prompt_template
-    ])
-
-
-
 
 
 # Load and define the embeddings and model to use
@@ -41,19 +26,26 @@ model = ChatOpenAI(model_name="gpt-4-turbo")
 vector_store = FAISS.load_local("faiss_index", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
 
 # Convert FAISS vector index into retriever
-retriever = vector_store.as_retriever()
+retriever = vector_store.as_retriever(
+    search_type = "similarity",
+    search_kwargs = { "k": 3 }
+)
+
+
+custom_rag_prompt = PromptTemplate.from_template(template)
+rag_chain = (
+    { "context": retriever, "question": RunnablePassthrough()}
+    | custom_rag_prompt
+    | model 
+    | StrOutputParser()
+    )
+
 
 
 # Define a function to interact with the chain
-def chat_with_context(retriever, user_query):
+def vector_search_query(user_query):
     """Generate a response to a user query."""
-    chain = (
-        { "context": retriever, "question": RunnablePassthrough()}
-        | chat_prompt_template
-        | model 
-        | StrOutputParser()
-        )
-    return chain.invoke(user_query)
+    return rag_chain.invoke(user_query)
 
 
 
@@ -63,4 +55,4 @@ while True:
     if user_input.lower() in ["exit", "quit"]:
         break
     
-    print("Answer: ", chat_with_context(retriever, user_input))
+    print("Answer: ", vector_search_query(user_input))
